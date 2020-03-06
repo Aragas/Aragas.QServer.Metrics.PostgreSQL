52f3285dfd156f606acb305a7193d1b04c255990
Vitaly Mikhailov <personal@aragas.org>
2020-03-06 19:00:46 +0300

    Last changes

--- PostgreSQLMetricsCollector.cs
+++ PostgreSQLMetricsCollector.cs
@@ -1,5 +1,5 @@
 ﻿using App.Metrics;
-using App.Metrics.Histogram;
+using App.Metrics.Gauge;
 using App.Metrics.Timer;
 
 using Microsoft.Extensions.Logging;
@@ -12,86 +12,63 @@ using System.Threading.Tasks;
 
 namespace Aragas.QServer.Metrics
 {
-    public sealed class PostgreSQLMetricsCollector : IMetricsCollector
+    public sealed class PostgreSQLMetricsCollector : BaseMetricsCollector
     {
-        private readonly HistogramOptions postgresql_response_milliseconds;
-        private readonly TimerOptions postgresql_last_response_milliseconds;
+        private readonly TimerOptions postgresql_response_milliseconds;
+        private readonly GaugeOptions postgresql_last_response_milliseconds;
 
         private readonly string _connectionString;
         private readonly ILogger _logger;
-        private readonly CancellationTokenSource _cancellationToken;
-        private readonly Task _collector;
-        private IMetrics _metrics;
 
-        public PostgreSQLMetricsCollector(string connectionName, string connectionString, ILogger<PostgreSQLMetricsCollector> logger)
+        public PostgreSQLMetricsCollector(IMetrics metrics, string connectionName, string connectionString, ILogger<PostgreSQLMetricsCollector> logger) : base(metrics)
         {
             _connectionString = connectionString;
             _logger = logger;
 
-            postgresql_response_milliseconds = new HistogramOptions()
+            postgresql_response_milliseconds = new TimerOptions()
             {
                 Name = $"postgresql_{connectionName}_response_milliseconds",
                 Context = "service",
                 MeasurementUnit = Unit.Custom("Milliseconds")
             };
-            postgresql_last_response_milliseconds = new TimerOptions()
+            postgresql_last_response_milliseconds = new GaugeOptions()
             {
                 Name = $"postgresql_{connectionName}_last_response_milliseconds",
                 Context = "service",
                 MeasurementUnit = Unit.Custom("Milliseconds")
             };
 
-            _cancellationToken = new CancellationTokenSource();
-            _collector = Task.Run(CheckMetrics);
         }
-
-        private async Task CheckMetrics()
+        public override async ValueTask UpdateAsync(CancellationToken stoppingToken)
         {
-            while (_cancellationToken?.IsCancellationRequested == false)
+            var timer = Metrics.Provider.Timer.Instance(postgresql_response_milliseconds);
+            var gauge = Metrics.Provider.Gauge.Instance(postgresql_last_response_milliseconds);
+            try
             {
-                var timer = _metrics.Provider.Timer.Instance(postgresql_last_response_milliseconds);
-                var start = timer.StartRecording();
-                var exception = false;
-                try
-                {
-                    await using var connection = new NpgsqlConnection(_connectionString);
-                    await connection.OpenAsync(_cancellationToken.Token);
+                using var cancellationTokenSource = new CancellationTokenSource(2000);
+                var cts = CancellationTokenSource.CreateLinkedTokenSource(stoppingToken, cancellationTokenSource.Token);
 
-                    await using var command = connection.CreateCommand();
-                    command.CommandText = "SELECT 1;";
-                    await command.ExecuteScalarAsync(_cancellationToken.Token);
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogWarning(ex, "Exception caught!", GetType().Name);
-                    exception = true;
-                }
+                var start = timer.StartRecording();
+                await using var connection = new NpgsqlConnection(_connectionString);
+                await connection.OpenAsync(cts.Token);
+
+                await using var command = connection.CreateCommand();
+                command.CommandText = "SELECT 1;";
+                await command.ExecuteScalarAsync(cts.Token);
                 var stop = timer.EndRecording();
 
-                if (exception)
-                {
-                    timer.Record(long.MaxValue, TimeUnit.Nanoseconds);
-                }
-                else
-                {
-                    var value = stop - start;
-                    timer.Record(value, TimeUnit.Nanoseconds);
-                    _metrics.Provider.Histogram.Instance(postgresql_response_milliseconds).Update(value);
-                }
-
-                await Task.Delay(5000);
+                var value = stop - start;
+                timer.Record(value, TimeUnit.Nanoseconds);
+                gauge.SetValue(value);
             }
-
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex, "Exception caught!", GetType().Name);
+                //timer.Record(0, TimeUnit.Nanoseconds);
+                gauge.SetValue(double.NaN);
+            }
         }
 
-        public void UpdateMetrics(IMetrics metrics)
-        {
-            _metrics = metrics;
-        }
-
-        public void Dispose()
-        {
-            _cancellationToken.Cancel();
-        }
+        public override void Dispose() { }
     }
 }
--- ServiceCollectionExtensions.cs
+++ ServiceCollectionExtensions.cs
@@ -1,32 +1,23 @@
 ﻿using App.Metrics;
+
 using Aragas.QServer.Metrics;
 using Aragas.QServer.Metrics.BackgroundServices;
+
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
 using System.Collections.Generic;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddNpgSqlMetrics(this IServiceCollection services, string connectionName, string connectionString)
-        {
-            return services.AddSingleton<IHostedService>(sp => new PostgreSQLMetricsService(
-                sp.GetRequiredService<IMetrics>(),
-                connectionName,
-                connectionString,
-                sp.GetRequiredService<ILogger<PostgreSQLMetricsService>>()));
-        }
-
-        public static IServiceCollection AddNpgSqlMetricsNew(this IServiceCollection services, string connectionName, string connectionString, int delay = 5000)
+        public static IServiceCollection AddNpgSqlMetrics(this IServiceCollection services, string connectionName, string connectionString, int delay = 5000)
         {
             services.AddSingleton<IHostedService, MetricsCollectorService>(sp => new MetricsCollectorService(
                 sp.GetRequiredService<IMetrics>(),
                 sp.GetRequiredService<ILogger<MetricsCollectorService>>(),
-                new List<IMetricsCollector>()
-                {
-                    ActivatorUtilities.CreateInstance<PostgreSQLMetricsCollector>(sp, connectionName, connectionString)
-                },
+                new List<BaseMetricsCollector> { ActivatorUtilities.CreateInstance<PostgreSQLMetricsCollector>(sp, connectionName, connectionString) },
                 delay));
 
             return services;
